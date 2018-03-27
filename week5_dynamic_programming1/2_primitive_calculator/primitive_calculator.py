48defba8acc498a7bf94d5556955ed2570939886
Vladyslav Melnyk <vladyslav.melnyk@gettyimages.com>
2018-03-27 23:21:44 +0300

    week 5 ex. 2

--- week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
+++ week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
@@ -1,21 +1,62 @@
 # Uses python3
 import sys
+from math import inf
 
-def optimal_sequence(n):
+
+def func1(x): return x - 1
+
+
+def func2(x): return x / 2
+
+
+def func3(x): return x / 3
+
+
+actions = [func1, func2, func3]
+
+
+def optimal_sequence(m):
+    min_sequence = [None] * (m + 1)
+
+    min_sequence[0] = 0
+    min_sequence[1] = 0
+
     sequence = []
-    while n >= 1:
-        sequence.append(n)
-        if n % 3 == 0:
-            n = n // 3
-        elif n % 2 == 0:
-            n = n // 2
-        else:
-            n = n - 1
-    return reversed(sequence)
+    sequence.append([0])
+    sequence.append([1])
 
-input = sys.stdin.read()
-n = int(input)
-sequence = list(optimal_sequence(n))
-print(len(sequence) - 1)
-for x in sequence:
-    print(x, end=' ')
+
+    # 1 2 3 4 5 6 7 8 9 10
+    # 1 1 1 2 3 2 3 3 2 3
+    for i in range(2, m + 1):
+        min_sequence[i] = inf
+        tmp2 = inf
+        tmp3 = inf
+        if (i % 3 == 0):
+            tmp3 = min_sequence[int(i / 3)] + 1
+        if (i % 2 == 0):
+            tmp2 = min_sequence[int(i / 2)] + 1
+        tmp1 = min_sequence[i - 1] + 1
+
+        min_tmp = min(tmp1, tmp2, tmp3)
+
+        min_sequence[i] = min_tmp
+        if (min_tmp == tmp1):
+            sequence.append(sequence[i-1] +[i])
+            continue
+        if (min_tmp == tmp2):
+            sequence.append(sequence[int(i / 2)]+[i])
+            continue
+        if (min_tmp == tmp3):
+            sequence.append(sequence[int(i / 3)]+[i])
+
+    return sequence[-1]
+
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    n = int(input)
+    sequence = list(optimal_sequence(n))
+    print(len(sequence) - 1)
+    for x in sequence:
+        print(x, end=' ')
