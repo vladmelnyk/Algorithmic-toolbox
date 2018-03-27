f96447a30eed159ead2065a1ddd5bdfb82f8f444
Vladyslav Melnyk <vladyslav.melnyk@gettyimages.com>
2018-03-27 23:24:34 +0300

    minor refactoring

--- week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
+++ week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
@@ -3,13 +3,13 @@ import sys
 from math import inf
 
 
-def func1(x): return x - 1
+def func1(x): return (int)(x - 1)
 
 
-def func2(x): return x / 2
+def func2(x): return (int)(x / 2)
 
 
-def func3(x): return x / 3
+def func3(x): return (int)(x / 3)
 
 
 actions = [func1, func2, func3]
@@ -25,7 +25,6 @@ def optimal_sequence(m):
     sequence.append([0])
     sequence.append([1])
 
-
     # 1 2 3 4 5 6 7 8 9 10
     # 1 1 1 2 3 2 3 3 2 3
     for i in range(2, m + 1):
@@ -33,22 +32,22 @@ def optimal_sequence(m):
         tmp2 = inf
         tmp3 = inf
         if (i % 3 == 0):
-            tmp3 = min_sequence[int(i / 3)] + 1
+            tmp3 = min_sequence[func3(i)] + 1
         if (i % 2 == 0):
-            tmp2 = min_sequence[int(i / 2)] + 1
+            tmp2 = min_sequence[func2(i)] + 1
         tmp1 = min_sequence[i - 1] + 1
 
         min_tmp = min(tmp1, tmp2, tmp3)
 
         min_sequence[i] = min_tmp
-        if (min_tmp == tmp1):
-            sequence.append(sequence[i-1] +[i])
+        if min_tmp == tmp1:
+            sequence.append(sequence[func1(i)] + [i])
             continue
-        if (min_tmp == tmp2):
-            sequence.append(sequence[int(i / 2)]+[i])
+        if min_tmp == tmp2:
+            sequence.append(sequence[func2(i)] + [i])
             continue
-        if (min_tmp == tmp3):
-            sequence.append(sequence[int(i / 3)]+[i])
+        if min_tmp == tmp3:
+            sequence.append(sequence[func3(i)] + [i])
 
     return sequence[-1]
 
