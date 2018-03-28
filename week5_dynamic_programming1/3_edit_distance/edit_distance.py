ba15d56f6ab164ef4036dbf712276e4efb2cc52b
Vladyslav Melnyk <vladyslav.melnyk@gettyimages.com>
2018-03-28 18:02:24 +0300

    week 5, ex. 3

--- week5_dynamic_programming1/3_edit_distance/edit_distance.py
+++ week5_dynamic_programming1/3_edit_distance/edit_distance.py
@@ -1,7 +1,28 @@
 # Uses python3
+import numpy as np
+import sys
+
+
 def edit_distance(s, t):
-    #write your code here
-    return 0
+    temp = np.zeros(shape=(len(s) + 1, len(t) + 1), dtype=int)
+
+    for i in range(len(s)+1):
+        temp[i, 0] = i
+    for i in range(len(t)+1):
+        temp[0, i] = i
+
+    for i in range(1, len(s) + 1):
+        for j in range(1, len(t) + 1):
+            if s[i - 1] == t[j - 1]:
+                temp[i, j] = temp[i - 1, j - 1]
+            else:
+                temp[i, j] = 1 + min(temp[i - 1, j], temp[i - 1, j - 1], temp[i, j - 1])
+
+    return (temp[len(s), len(t)])
+
 
 if __name__ == "__main__":
-    print(edit_distance(input(), input()))
+    input = sys.stdin.read()
+    input = list(map(str, input.split('\n')))
+    # print(edit_distance(input(), input()))
+    print(edit_distance(input[0], input[1]))
