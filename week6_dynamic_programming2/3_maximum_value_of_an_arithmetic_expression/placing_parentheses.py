1d8fcb78683d5b1159e2efc003dd05d24fa6131c
Vladyslav Melnyk <vladyslav.melnyk@gettyimages.com>
2018-03-28 22:50:12 +0300

    week 6, ex3

--- week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
+++ week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
@@ -1,4 +1,9 @@
 # Uses python3
+from math import inf
+
+import numpy as np
+
+
 def evalt(a, b, op):
     if op == '+':
         return a + b
@@ -9,9 +14,38 @@ def evalt(a, b, op):
     else:
         assert False
 
+
+def min_and_max(m, M, op, i, j):
+    tmp_min = inf
+    tmp_max = -inf
+
+    for k in range(i, j):
+        a = evalt(M[i][k], m[k + 1][j], op[k])
+        b = evalt(m[i][k], m[k + 1][j], op[k])
+        c = evalt(M[i][k], M[k + 1][j], op[k])
+        d = evalt(m[i][k], M[k + 1][j], op[k])
+
+        tmp_min = min(tmp_min, a, b, c, d)
+        tmp_max = max(tmp_max, a, b, c, d)
+
+    return (tmp_min, tmp_max)
+
+
 def get_maximum_value(dataset):
-    #write your code here
-    return 0
+    op = dataset[1:len(dataset):2]
+    d = dataset[0:len(dataset) + 1:2]
+    n = len(d)
+    m = np.zeros(shape=(n, n), dtype=int)
+    M = np.zeros(shape=(n, n), dtype=int)
+    for i in range(n):
+        m[i][i] = int(d[i])
+        M[i][i] = int(d[i])
+
+    for s in range(1, n):
+        for i in range(n - s):
+            j = i + s
+            m[i][j], M[i][j] = min_and_max(m, M, op, i, j)
+    return M[0][n - 1]
 
 
 if __name__ == "__main__":
