6276fbf31a855ea45dcd0b0b39dbb5e9eb52a95d
Vladyslav Melnyk <vladyslav.melnyk@gettyimages.com>
2018-03-28 21:46:14 +0300

    week 6, ex1

--- week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
+++ week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
@@ -1,13 +1,22 @@
 # Uses python3
 import sys
 
+import numpy as np
+
+
 def optimal_weight(W, w):
     # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+
+    matrix = np.zeros(shape=(len(w), W + 1), dtype=int)
+
+    for j in range(0, len(w)):
+        for i in range(1, W + 1):
+            if w[j] > i:
+                matrix[j, i] = matrix[j - 1, i]
+            else:
+                matrix[j, i] = max(w[j] + matrix[j - 1, i - w[j]], matrix[j - 1, i])
+    return matrix[len(w) - 1, W]
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
